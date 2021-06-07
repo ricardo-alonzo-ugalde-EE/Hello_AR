c94dd45d2053a0722383841588d838b7fb941a86
Ricardo Alonzo Ugalde <ricardo.alonzo.ugalde@gmail.com>
2021-06-06 20:22:33 -0700

    some methods

--- app/src/main/java/org/electricuniverse/hello_ar/MainActivity.java
+++ app/src/main/java/org/electricuniverse/hello_ar/MainActivity.java
@@ -2,13 +2,54 @@ package org.electricuniverse.hello_ar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.ar.sceneform.ux.ArFragment;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity
+{
+    // object of ArFragment Class
+    private ArFragment arCam;
+
+    // helps to render the 3d model
+    // only once when we tap the screen
+    private int clickNo = 0;
+
+    public static boolean checkSystemSupport(Activity activity) {
+
+        // checking whether the API version of the running Android >= 24
+        // that means Android Nougat 7.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            String openGlVersion = ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE))).getDeviceConfigurationInfo().getGlEsVersion();
+
+            // checking whether the OpenGL version >= 3.0
+            if (Double.parseDouble(openGlVersion) >= 3.0) {
+                return true;
+            } else {
+                Toast.makeText(activity, "App needs OpenGl Version 3.0 or later", Toast.LENGTH_SHORT).show();
+                activity.finish();
+                return false;
+            }
+        } else {
+            Toast.makeText(activity, "App does not support required Build Version", Toast.LENGTH_SHORT).show();
+            activity.finish();
+            return false;
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
